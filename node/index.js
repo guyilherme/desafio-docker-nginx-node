b23cd4324a29e84dd7092988897587c15198dbdc
Guilherme Macêdo da Cruz <eu@guilhermecruz.dev>
2021-02-22 23:18:16 -0300

    Iniciando listangem

--- node/index.js
+++ node/index.js
@@ -13,11 +13,19 @@ const connection = mysql.createConnection(config)
 const sql = 'INSERT INTO pessoas(name) values ("João")'
 
 connection.query(sql)
-connection.end
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
-})
+// app.get('/', (req,res) => {
+//     res.send('<h1>Full Cycle Rocks!</h1>')
+// })
+
+app.get('/', function(req, res) {
+    var sql='SELECT name FROM pessoas'
+    connection.query(sql, function (err, data, fields) {
+    if (err) throw err
+    res.send(data)
+    connection.end
+  });
+});
 
 app.listen(port, () => {
     console.log('Rodando na porta ' + port)
